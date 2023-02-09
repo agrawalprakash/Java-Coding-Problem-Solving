505b9cbc0a51763509391ae3dc7f04255f43f798
agrawalprakash <33377123+agrawalprakash@users.noreply.github.com>
2023-02-09 07:18:04 +0530

    Prime Number Check

---
+++ PrimeNumberCheck.java
@@ -0,0 +1,32 @@
+package main;
+
+public class PrimeNumberCheck {
+	
+	public static void main (String[] args) {
+		
+		System.out.println(isPrime(19));
+		System.out.println(isPrime(49));
+		
+	}
+
+	private static boolean isPrime(int n) {
+		
+		if (n == 0 || n == 1) {
+			
+			return false;
+		}
+		
+		if ( n == 2 ) { 
+			return true;
+		}
+		
+		for (int i = 2; i <= n/2; i++) {
+			if ( n % i == 0) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
+
+}
