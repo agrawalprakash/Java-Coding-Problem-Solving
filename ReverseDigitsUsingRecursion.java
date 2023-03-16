61229d5f4e8add5aa6c4f7f0218a67461471ca78
agrawalprakash <33377123+agrawalprakash@users.noreply.github.com>
2023-03-16 11:37:42 +0530

    Optimized Reverse Digits using Recursion

--- ReverseDigitsUsingRecursion.java
+++ ReverseDigitsUsingRecursion.java
@@ -18,11 +18,29 @@ public class ReverseDigitsUsingRecursion {
 		
 	}
 	
+	static int reverse2(int n) {
+		
+		int digits = (int)(Math.log10(n)) + 1;
+		
+		return helper(n, digits);
+		
+	}
+	
+	private static int helper(int n, int digits) {
+		
+		if (n % 10 == n) {
+			return n;
+		}
+		int rem = n%10;
+		return rem * (int)Math.pow(10, digits-1) + helper(n/10, digits-1);
+		
+	}
+	
+	
+	
 	public static void main (String[] args) {
 		
-		reverse(14789);
-		
-		System.out.println("Reverse of number 14789 is " + sum);
+		System.out.println("Reverse of number 14789 is " + reverse2(14789));
 	}
 
 }
